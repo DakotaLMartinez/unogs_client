08321f10a754a342ea959e92f811208c6cc66b99
DakotaLMartinez <dakotaleemusic@gmail.com>
2020-06-23 17:38:30 -0700

    initial commit

---
+++ lib/unogs_client.rb
@@ -0,0 +1,7 @@
+require "unogs_client/version"
+require "unogs_client/api"
+
+module UnogsClient
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/unogs_client/version.rb
@@ -0,0 +1,3 @@
+module UnogsClient
+  VERSION = "0.1.0"
+end
